375daf7ce53e60cac323bb130ec4203d9991e6d7
heduardo91 <hecardenas91@gmail.com>
2026-07-25 12:49:21 -0500

    Enhance dashboard with separator and average sales chart
    Added a horizontal line separator and a new chart for average sales.

--- dashboard.py
+++ dashboard.py
@@ -12,8 +12,9 @@ df = cargar_datos() # UTILIZANDO LA FUNCIÓN QUE NOS DEVUELVE EL DATAFRAME (DF)
 st.set_page_config(page_title = "Wigo Motors", 
                    layout="wide")      
 
-st.title("WIGO MOTORS S.A.C.")                      
-st.subheader("Buscador comercial") 
+st.markdown("""
+<hr style="height:3px;border:none;background:#1E88E5;">
+""", unsafe_allow_html=True)
 
 st.sidebar.header("Buscador")
 tipo_busqueda = st.sidebar.selectbox("Seleccione tipo de búsqueda", ["Marca", "Asesor comercial", "Sede"])  
